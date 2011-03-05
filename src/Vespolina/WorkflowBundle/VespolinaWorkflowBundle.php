aa4f845e6a2191ce8e4b928d5a16d00ce2fb7352
IamPersistent <develop@zestic.com>
2011-03-04 23:42:14 -0800

    add emtpy WorkflowBundle

---
+++ src/Vespolina/WorkflowBundle/VespolinaWorkflowBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2011 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\WorkflowBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VespolinaWorkflowBundle extends Bundle
+{
+
+}
