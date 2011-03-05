f6cfd4d3f6af08fda0d87aa9700b30eaacd8d498
IamPersistent <develop@zestic.com>
2011-03-04 23:23:22 -0800

    add empty OrderBundle

---
+++ src/Vespolina/OrderBundle/VespolinaOrderBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2011 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\OrderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VespolinaOrderBundle extends Bundle
+{
+
+}
