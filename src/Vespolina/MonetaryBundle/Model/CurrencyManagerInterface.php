a80eab1ba13539422a96edb68a68de104997aeca
IamPersistent <develop@zestic.com>
2011-03-21 02:09:03 -0700

    add copyright notices

--- src/Vespolina/MonetaryBundle/Model/CurrencyManagerInterface.php
+++ src/Vespolina/MonetaryBundle/Model/CurrencyManagerInterface.php
@@ -1,10 +1,18 @@
 <?php
-
+/**
+ * (c) 2011 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 namespace Vespolina\MonetaryBundle\Model;
 
 use Vespolina\MonetaryBundle\Model\CurrencyInterface;
 use Vespolina\MonetaryBundle\Model\MonetaryInterface;
 
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
 interface CurrencyManagerInterface
 {
     /**
--- src/Vespolina/MonetaryBundle/VespolinaMonetaryBundle.php
+++ src/Vespolina/MonetaryBundle/VespolinaMonetaryBundle.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * (c) 2011 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Vespolina\MonetaryBundle;
 
