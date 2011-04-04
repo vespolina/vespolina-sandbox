ecdd3c22384cd4b0edcdca33a6e7630956ee4ffa
IamPersistent <develop@zestic.com>
2011-04-04 14:48:26 -0700

    add currency exhange interface

---
+++ src/Vespolina/MonetaryBundle/Service/CurrencyExchangeInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * (c) 2011 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\MonetaryBundle\Service;
+
+use Vespolina\MonetaryBundle\Model\CurrencyInterface;
+
+interface CurrencyExchangeInterface
+{
+    /**
+     * Get the exchange rate from on currency to another.  $from and $to can be an
+     * ISO 4217 currency code or a Currency object
+     * 
+     * @param  mixed $from string | Vespolina\MonetaryBundle\Model\CurrencyInterface
+     * @param  mixed $to string | Vespolina\MonetaryBundle\Model\CurrencyInterface
+     * @return float
+     */
+    public function getExchangeRate($from, $to);
+}
