d4b776c25cf35d179977a29d7aad334ad9700df5
IamPersistent <develop@zestic.com>
2011-04-07 23:38:52 -0700

    remove exchange rate related functionality and add name functionality

--- src/Vespolina/MonetaryBundle/Model/CurrencyInterface.php
+++ src/Vespolina/MonetaryBundle/Model/CurrencyInterface.php
@@ -18,6 +18,16 @@ interface CurrencyInterface
     public function __construct();
 
     /**
+     * Return the name of the currency
+     */
+    public function getName();
+
+    /**
+     * Return the short name of the currency
+     */
+    public function getShortName();
+
+    /**
      * Get the ISO 4217 currency code of this currency.
      *
      * @return string
@@ -46,37 +56,4 @@ interface CurrencyInterface
      * @return string
      */
     public function formatAmount($amount);
-
-    /**
-     * Return the exchange rate, based on base currency
-     *
-     * @return mixed $rate
-     */
-    public function getExchangeRate();
-
-    /**
-     * Return the date and time of the exchange rate
-     *
-     * @return \DateTime
-     */
-    public function getExchangeDateTime();
-
-    /**
-     * Return the base currency for the exchange rate
-     *
-     * @return Vespolina\MonetaryBundle\Model\CurrencyInterface
-     */
-    public function getBaseCurrency();
-
-    /**
-     * Return the value after the currency conversion, this can be an instance of
-     * Vespolina\MonetaryBundle\Model\MonetaryInterface.  If a Monetary object is passed in as the amount
-     * an new instance of Vespolina\MonetaryBundle\Model\MonetaryInterface, with this currency is returned
-     *
-     * Is returning Vespolina\MonetaryBundle\Model\MonetaryInterface the right behavior? should the manager handle this?
-     *
-     * @param mixed $amount
-     * @return mixed
-     */
-    public function exchange($amount);
 }
