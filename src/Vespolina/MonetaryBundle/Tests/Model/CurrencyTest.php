9bc526ae664e537823e04c857b819aa30fb75f42
IamPersistent <develop@zestic.com>
2011-04-15 04:39:31 -0700

    update currency test to reflect change in currency used for testing

--- src/Vespolina/MonetaryBundle/Tests/Model/CurrencyTest.php
+++ src/Vespolina/MonetaryBundle/Tests/Model/CurrencyTest.php
@@ -7,23 +7,20 @@ use Vespolina\MonetaryBundle\Tests\MonetaryTestBase;
 class CurrencyTest extends MonetaryTestBase
 {
     protected $currency;
-    protected $largerCurrency;
-    protected $smallerCurrency;
-    protected $startTime;
 
     public function testGetCurrencyCode()
     {
-        $this->assertSame('EUR', $this->currency->getCurrencyCode(), 'the currency code of the currency');
+        $this->assertSame('XTS', $this->currency->getCurrencyCode(), 'the currency code of the currency');
     }
 
     public function testGetName()
     {
-        $this->assertSame('Euro', $this->currency->getName(), 'the name of the currency');
+        $this->assertSame('Codes specifically reserved for testing purpose', $this->currency->getName(), 'the name of the currency');
     }
 
     public function testGetSymbol()
     {
-        $this->assertSame('€', $this->currency->getSymbol(), 'the symbol for the currency');
+        $this->assertSame('Z', $this->currency->getSymbol(), 'the symbol for the currency');
     }
 
     public function testGetPrecision()
@@ -33,7 +30,7 @@ class CurrencyTest extends MonetaryTestBase
 
     public function testFormatAmount()
     {
-        $this->assertSame('€3.21', $this->currency->formatAmount(3.205), 'amount, formatted for the currency');
+        $this->assertSame('Z3.21', $this->currency->formatAmount(3.205), 'amount, formatted for the currency');
     }
 
     public function setUp()
