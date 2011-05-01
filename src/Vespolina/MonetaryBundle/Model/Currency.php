d3085e0ab082623879873e2aaa21190349b60662
IamPersistent <develop@zestic.com>
2011-04-30 22:18:19 -0700

    change currency base class to fit generated currencies

--- src/Vespolina/MonetaryBundle/Model/Currency.php
+++ src/Vespolina/MonetaryBundle/Model/Currency.php
@@ -14,63 +14,17 @@ use Vespolina\MonetaryBundle\Model\CurrencyInterface;
  */
 abstract class Currency implements CurrencyInterface
 {
-    protected $currencyCode;
-    protected $name;
-    protected $precision;
-    protected $shortName;
-    protected $symbol;
-
-    /**
-     * @inheritdoc
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getShortName()
-    {
-        return $this->shortName;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getCurrencyCode()
-    {
-        return $this->currencyCode;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getSymbol()
-    {
-        return $this->symbol;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getPrecision()
-    {
-        return $this->precision;
-    }
-
     /**
      * @inheritdoc
      */
     public function formatAmount($amount)
     {
-        return sprintf('%s%s', $this->symbol, $this->rounding($amount));
+        return sprintf('%s%s', $this->getSymbol(), $this->rounding($amount));
     }
 
     protected function rounding($amount)
     {
-        $roundUp = '.'.substr('0000000000000005', -($this->precision+1));
-        return bcadd((string)$amount, $roundUp, $this->precision);
+        $roundUp = '.'.substr('0000000000000005', -($this->getPrecision() + 1));
+        return bcadd((string)$amount, $roundUp, $this->getPrecision());
     }
 }
