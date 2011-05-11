615047f6bbb2ad5aac1e8f8f48f68d5381d93915
IamPersistent <develop@zestic.com>
2011-05-11 07:41:18 -0700

    add some configuration

---
+++ src/Vespolina/MonetaryBundle/DependencyInjection/VespolinaMonetaryExtension.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * (c) Vespolina Project http://www.vespolina-project.org
+ *
+ * (c) Daniel Kucharski <daniel@xerias.be>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\MonetaryBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+class VespolinaMonetaryExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('monetary.xml');
+    }
+}
