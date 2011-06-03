bb339821018dee6711b792e6fe3c450f0f233c77
inspiran <daniel@inspiran.be>
2011-06-04 01:04:33 +0200

    - Implemented workflow activity execution for zeta components worklows (ez service object) - Implemented execution method of the workflow service

---
+++ vendor/bundles/Vespolina/WorkflowBundle/Model/EcWorkflow/EcWorkflowActivity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: inspiran
+ * Date: 4/06/11
+ * Time: 00:33
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class EcWorkflowActivity {
+
+}
