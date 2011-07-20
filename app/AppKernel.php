c45b0b87c19611db3ab99b08a11e767dafbcd3c3
inspiran <daniel@inspiran.be>
2011-07-20 21:59:32 +0200

    Added TaxationBundle to AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,11 +16,15 @@ class AppKernel extends Kernel
             new Application\DefaultBundle\DefaultBundle(),
             
             new Vespolina\CoreBundle\VespolinaCoreBundle(),
+            new Vespolina\CartBundle\VespolinaCartBundle(),
             new Vespolina\DocumentBundle\VespolinaDocumentBundle(),
+            new Vespolina\EcommerceFlowsBundle\VespolinaEcommerceFlowsBundle(),
+            new Vespolina\MerchandiseBundle\VespolinaMerchandiseBundle(),
             new Vespolina\MonetaryBundle\VespolinaMonetaryBundle(),
             new Vespolina\OrderBundle\VespolinaOrderBundle(),
             new Vespolina\PartnerBundle\VespolinaPartnerBundle(),
             new Vespolina\PricingBundle\VespolinaPricingBundle(),
+            new Vespolina\TaxationBundle\VespolinaTaxationBundle(),
             new Vespolina\WorkflowBundle\VespolinaWorkflowBundle(),
 
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
