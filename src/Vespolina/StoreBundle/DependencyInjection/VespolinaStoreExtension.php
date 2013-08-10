787274e206e472b13f453ffd6df09f4f7601b57c
Luis Cordova <cordoval@gmail.com>
2013-08-09 21:14:23 -0500

    add extension

---
+++ src/Vespolina/StoreBundle/DependencyInjection/VespolinaStoreExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vespolina\StoreBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class VespolinaStoreExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+
+    }
+}
