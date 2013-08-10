aa328b9579d177fe0f704014d757aa07aec2d319
Luis Cordova <cordoval@gmail.com>
2013-08-09 19:04:07 -0500

    plug script to check for temporary work on bundles

---
+++ check_vendors.sh
@@ -0,0 +1,89 @@
+cd vendor/vespolina
+
+cd commerce-bundle/Vespolina/CommerceBundle
+echo '>> commerce-bundle/Vespolina/CommerceBundle'
+git branch
+git status
+echo "\n"
+
+cd ../../../
+
+cd core
+echo '>> core'
+git branch
+git status
+echo "\n"
+
+cd ..
+
+cd admin-bundle/Vespolina/AdminBundle
+echo '>> admin-bundle/Vespolina/AdminBundle'
+git branch
+git status
+echo "\n"
+
+cd ../../../
+
+cd molino
+echo '>> molino'
+git branch
+git status
+echo "\n"
+
+cd ..
+
+cd order
+echo '>> order'
+git branch
+git status
+echo "\n"
+
+cd ..
+
+cd partner
+echo '>> partner'
+git branch
+git status
+echo "\n"
+
+cd ..
+
+cd pricing
+echo '>> pricing'
+git branch
+git status
+echo "\n"
+
+cd ..
+
+cd product
+echo '>> product'
+git branch
+git status
+echo "\n"
+
+cd ..
+
+cd store-bundle/Vespolina/StoreBundle
+echo '>> store-bundle/Vespolina/StoreBundle'
+git branch
+git status
+echo "\n"
+
+cd ../../../
+
+cd taxation-bundle/Vespolina/TaxationBundle
+echo '>> taxation-bundle/Vespolina/TaxationBundle'
+git branch
+git status
+echo "\n"
+
+cd ../../../
+
+cd taxonomy
+echo '>> taxonomy'
+git branch
+git status
+echo "\n"
+
+cd ../../
