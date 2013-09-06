284d335b859bf8d4272d9532f1321c1ad803f3d4
inspiran <daniel@xerias.be>
2013-09-06 23:12:30 +0200

    Added a new process step to register action definitions

---
+++ src/Vespolina/DefaultStoreBundle/ProcessScenario/Setup/Step/CreateActionDefinitions.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * (c) 2011 - ∞ Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\DefaultStoreBundle\ProcessScenario\Setup\Step;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Vespolina\Entity\Action\ActionDefinition;
+use Vespolina\Entity\Partner\Partner;
+
+class CreateActionDefinitions extends AbstractSetupStep
+{
+    public function execute(&$context) {
+
+        $actionManager = $this->getContainer()->get('vespolina.action_manager');
+        $actionDefinitions = array();
+        $actionDefinitionFixtures = array(
+            array('name' => 'notify_customer',
+                'topic'  => 'order',
+                'eventName' => 'v.action.order.notification_customer'));
+
+        foreach($actionDefinitionFixtures as $fixture) {
+
+            $actionDefinition = new ActionDefinition($fixture['name'], $fixture['topic']);
+            $actionDefinition->setEventName($fixture['eventName']);
+            $actionManager->addActionDefinition($actionDefinition);
+            $actionManager->updateActionDefinition($actionDefinition);
+
+
+            $actionDefinitions[] = $actionDefinition;
+        }
+        $this->getLogger()->addInfo('Setup ' . count($actionDefinitions) . ' action(s).' );
+    }
+
+    public function getName() {
+
+        return 'create_action_definitions';
+    }
+}
