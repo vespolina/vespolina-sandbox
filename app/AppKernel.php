f09ed32bc4dcc1d5e0a6354cb7120e63441feafc
Jarrett Croll <jarrett.croll@gmail.com>
2013-09-12 11:29:50 -0700

    some clean up

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
             new Application\UserBundle\UserBundle(),
-            new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
 
             //Basic bundles for an ecommerce store
             new Vespolina\CommerceBundle\VespolinaCommerceBundle(),
