851f4c96529e9c16ad9d27d3ae1b3add67a0d57b
Jarrett Croll <jarrett.croll@gmail.com>
2013-10-21 12:34:31 -0700

    update app_dev.php to 2.3

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,23 +1,25 @@
 <?php
-// if you don't want to setup permissions the proper way, just uncomment the following PHP line
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-// this check prevents access to debug front controllers that are deployed by accident to production servers.
-// feel free to remove this, extend it, or make something more sophisticated.
-//if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-//    '127.0.0.1',
-//    '192.168.56.1',
-//    '::1',
-//))) {
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+//if (isset($_SERVER['HTTP_CLIENT_IP'])
+//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+//) {
 //    header('HTTP/1.0 403 Forbidden');
 //    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 //}
 
-require_once __DIR__.'/../app/autoload.php';
-require_once __DIR__.'/../app/AppKernel.php';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
 
-use Symfony\Component\HttpFoundation\Request;
+require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
